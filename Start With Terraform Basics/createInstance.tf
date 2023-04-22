0a846eafef0e0fd48afcfdd9a53eacdb7dae0c13
110melanin <justicegaisie141@gmail.com>
2023-04-23 00:13:44 +0100

    adjustment

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,10 +1,10 @@
 
-resource "aws_instance" "MyFirstInstnace" {
-  count         = 3
-  ami           = "ami-0bff25b43a4479334"
-  instance_type = "t4g.micro"
+resource "aws_instance" "MyFirstInstance" {
+  count         = 2
+  ami           = "ami-0eb27879c20e8bf16"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "demoinstnce-${count.index}"
+    Name = "demoinstance-${count.index}"
   }
 }
