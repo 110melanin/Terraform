3ca44ae216fdcee25ecc6d723dcea3c3e64d400d
110melanin <justicegaisie141@gmail.com>
2023-04-26 17:17:08 +0100

    nat gateway and vpc change

--- Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/vpc.tf
+++ Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/vpc.tf
@@ -13,74 +13,74 @@ resource "aws_vpc" "terra-vpc" {
 
 # Create AWS Subnets (Subnet 1)
 
-resource "aws_subnet" "terra-vpc-public-1" {
+resource "aws_subnet" "terra-subnet-public-1" {
   vpc_id                  = aws_vpc.terra-vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "eu-west-2a"
 
   tags = {
-    Name = "terra-vpc-public-1"
+    Name = "terra-subnet-public-1"
   }
 }
 
 # Create AWS Subnets (Subnet 2)
-resource "aws_subnet" "terra-vpc-public-2" {
+resource "aws_subnet" "terra-subnet-public-2" {
   vpc_id                  = aws_vpc.terra-vpc.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "eu-west-2b"
 
   tags = {
-    Name = "terra-vpc-public-2"
+    Name = "terra-subnet-public-2"
   }
 }
 
 # Create AWS Subnets (Subnet 3)
-resource "aws_subnet" "terra-vpc-public-3" {
+resource "aws_subnet" "terra-subnet-public-3" {
   vpc_id                  = aws_vpc.terra-vpc.id
   cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "eu-west-2c"
 
   tags = {
-    Name = "terra-vpc-public-3"
+    Name = "terra-subnet-public-3"
   }
 }
 
 # Private Subnets in Custom VPC (Subnet 1)
-resource "aws_subnet" "terra-vpc-private-1" {
+resource "aws_subnet" "terra-subnet-private-1" {
   vpc_id                  = aws_vpc.terra-vpc.id
   cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "eu-west-2a"
 
   tags = {
-    Name = "terra-vpc-private-1"
+    Name = "terra-subnet-private-1"
   }
 }
 
 # (Subnet 2)
-resource "aws_subnet" "terra-vpc-private-2" {
+resource "aws_subnet" "terra-subnet-private-2" {
   vpc_id                  = aws_vpc.terra-vpc.id
   cidr_block              = "10.0.5.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "eu-west-2b"
 
   tags = {
-    Name = "terra-vpc-private-2"
+    Name = "terra-subnet-private-2"
   }
 }
 
 # (Subnet 3)
-resource "aws_subnet" "terra-vpc-private-3" {
+resource "aws_subnet" "terra-subnet-private-3" {
   vpc_id                  = aws_vpc.terra-vpc.id
   cidr_block              = "10.0.6.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "eu-west-2c"
 
   tags = {
-    Name = "terra-vpc-private-3"
+    Name = "terra-subnet-private-3"
   }
 }
 
@@ -108,16 +108,16 @@ resource "aws_route_table" "terra-public-rt" {
   }
 
 resource "aws_route_table_association" "terra-public-rt-1a" {
-  subnet_id      = aws_subnet.terra-vpc-public-1.id
+  subnet_id      = aws_subnet.terra-subnet-public-1.id
   route_table_id = aws_route_table.terra-public-rt.id
 }
 
 resource "aws_route_table_association" "terra-public-rt-2a" {
-  subnet_id      = aws_subnet.terra-vpc-public-2.id
+  subnet_id      = aws_subnet.terra-subnet-public-2.id
   route_table_id = aws_route_table.terra-public-rt.id
 }
 
 resource "aws_route_table_association" "terra-public-rt-3a" {
-  subnet_id      = aws_subnet.terra-vpc-public-3.id
+  subnet_id      = aws_subnet.terra-subnet-public-3.id
   route_table_id = aws_route_table.terra-public-rt.id
 }
