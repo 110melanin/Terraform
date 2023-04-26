3798e703d386559549527979eae1efc68f842e53
110melanin <justicegaisie141@gmail.com>
2023-04-26 06:11:52 +0100

    new vpc and new nat

--- Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/vpc.tf
+++ Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/vpc.tf
@@ -1,117 +1,123 @@
-#Create AWS VPC
-resource "aws_vpc" "levelupvpc" {
+# Create AWS VPC
+resource "aws_vpc" "terra-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
-  enable_dns_support   = "true"
+  enable_dns_support = "true"
   enable_dns_hostnames = "true"
-  enable_classiclink   = "false"
+  enable_classiclink = "false"
 
   tags = {
-    Name = "levelupvpc"
+    Name = "terra-vpc"
   }
 }
 
-# Public Subnets in Custom VPC
-resource "aws_subnet" "levelupvpc-public-1" {
-  vpc_id                  = aws_vpc.levelupvpc.id
+# Create AWS Subnets (Subnet 1)
+
+resource "aws_subnet" "terra-vpc-public-1" {
+  vpc_id                  = aws_vpc.terra-vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "us-east-2a"
+  availability_zone       = "eu-west-2a"
 
   tags = {
-    Name = "levelupvpc-public-1"
+    Name = "terra-vpc-public-1"
   }
 }
 
-resource "aws_subnet" "levelupvpc-public-2" {
-  vpc_id                  = aws_vpc.levelupvpc.id
+# Create AWS Subnets (Subnet 2)
+resource "aws_subnet" "terra-vpc-public-2" {
+  vpc_id                  = aws_vpc.terra-vpc.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "us-east-2b"
+  availability_zone       = "eu-west-2b"
 
   tags = {
-    Name = "levelupvpc-public-2"
+    Name = "terra-vpc-public-2"
   }
 }
 
-resource "aws_subnet" "levelupvpc-public-3" {
-  vpc_id                  = aws_vpc.levelupvpc.id
+# Create AWS Subnets (Subnet 3)
+resource "aws_subnet" "terra-vpc-public-3" {
+  vpc_id                  = aws_vpc.terra-vpc.id
   cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "us-east-2c"
+  availability_zone       = "eu-west-2c"
 
   tags = {
-    Name = "levelupvpc-public-3"
+    Name = "terra-vpc-public-3"
   }
 }
 
-# Private Subnets in Custom VPC
-resource "aws_subnet" "levelupvpc-private-1" {
-  vpc_id                  = aws_vpc.levelupvpc.id
+# Private Subnets in Custom VPC (Subnet 1)
+resource "aws_subnet" "terra-vpc-private-1" {
+  vpc_id                  = aws_vpc.terra-vpc.id
   cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "us-east-2a"
+  availability_zone       = "eu-west-2a"
 
   tags = {
-    Name = "levelupvpc-private-1"
+    Name = "terra-vpc-private-1"
   }
 }
 
-resource "aws_subnet" "levelupvpc-private-2" {
-  vpc_id                  = aws_vpc.levelupvpc.id
+# (Subnet 2)
+resource "aws_subnet" "terra-vpc-private-2" {
+  vpc_id                  = aws_vpc.terra-vpc.id
   cidr_block              = "10.0.5.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "us-east-2b"
+  availability_zone       = "eu-west-2b"
 
   tags = {
-    Name = "levelupvpc-private-2"
+    Name = "terra-vpc-private-2"
   }
 }
 
-resource "aws_subnet" "levelupvpc-private-3" {
-  vpc_id                  = aws_vpc.levelupvpc.id
+# (Subnet 3)
+resource "aws_subnet" "terra-vpc-private-3" {
+  vpc_id                  = aws_vpc.terra-vpc.id
   cidr_block              = "10.0.6.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "us-east-2c"
+  availability_zone       = "eu-west-2c"
 
   tags = {
-    Name = "levelupvpc-private-3"
+    Name = "terra-vpc-private-3"
   }
 }
 
-# Custom internet Gateway
-resource "aws_internet_gateway" "levelup-gw" {
-  vpc_id = aws_vpc.levelupvpc.id
+# Custom Internet Gateway
+resource "aws_internet_gateway" "terra-gw" {
+  vpc_id = aws_vpc.terra-vpc.id
 
   tags = {
-    Name = "levelup-gw"
+    Name = "terra-gw"
   }
 }
 
-#Routing Table for the Custom VPC
-resource "aws_route_table" "levelup-public" {
-  vpc_id = aws_vpc.levelupvpc.id
+# Custom Route Tables for CustomVPC
+
+resource "aws_route_table" "terra-public-rt" {
+  vpc_id = aws_vpc.terra-vpc.id
+
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.levelup-gw.id
+    gateway_id = aws_internet_gateway.terra-gw.id
   }
-
+}
   tags = {
-    Name = "levelup-public-1"
+    Name = "terra-public-rt-1"
   }
+
+resource "aws_route_table_association" "terra-public-rt-1a" {
+  subnet_id      = aws_subnet.terra-vpc-public-1.id
+  route_table_id = aws_route_table.terra-public-rt.id
 }
 
-resource "aws_route_table_association" "levelup-public-1-a" {
-  subnet_id      = aws_subnet.levelupvpc-public-1.id
-  route_table_id = aws_route_table.levelup-public.id
+resource "aws_route_table_association" "terra-public-rt-2a" {
+  subnet_id      = aws_subnet.terra-vpc-public-2.id
+  route_table_id = aws_route_table.terra-public-rt.id
 }
 
-resource "aws_route_table_association" "levelup-public-2-a" {
-  subnet_id      = aws_subnet.levelupvpc-public-2.id
-  route_table_id = aws_route_table.levelup-public.id
-}
-
-resource "aws_route_table_association" "levelup-public-3-a" {
-  subnet_id      = aws_subnet.levelupvpc-public-3.id
-  route_table_id = aws_route_table.levelup-public.id
+resource "aws_route_table_association" "terra-public-rt-3a" {
+  subnet_id      = aws_subnet.terra-vpc-public-3.id
+  route_table_id = aws_route_table.terra-public-rt.id
 }
