f87d11386b5d61b84bc445efc9be0f8dbbf73424
110melanin <justicegaisie141@gmail.com>
2023-04-27 03:22:59 +0100

    cidr block edit again

--- Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/security_group.tf
+++ Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/security_group.tf
@@ -3,14 +3,14 @@
 resource "aws_security_group" "allow-terra-ssh" {
   name        = "allow_terra_ssh"
   description = "security group to allow ssh connections"
-  vpc_id      = aws_vpc.terra-vpc.id
+  vpc_id      = aws_vpc.terra-vpc.cidr_block
 
   ingress {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.terra-vpc.id]
-    ipv6_cidr_blocks = [aws_vpc.terra-vpc.id]
+    cidr_blocks      = [aws_vpc.terra-vpc.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.terra-vpc.cidr_block]
   }
 
   egress {
