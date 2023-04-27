abc3f976de3cfd3fa8b728d640a075e6d81bef90
110melanin <justicegaisie141@gmail.com>
2023-04-27 04:08:25 +0100

    subnet modified

--- Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/createInstance.tf
+++ Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/createInstance.tf
@@ -8,8 +8,13 @@ resource "aws_instance" "terra-instance" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
   key_name      = aws_key_pair.terra_key.key_name
+  
+  aws_security_group_id = [aws_security_group.allow-terra-ssh.id]
+  subnet_id = aws_subnet.terra-subnet-public-1.id    
+
 
   tags = {
     Name = "terra_instance"
   }
 }
+
