e1116e377d79d844bd4ba74cc3cbbac8fb8fb4ee
110melanin <justicegaisie141@gmail.com>
2023-04-27 03:10:40 +0100

    edited SG

---
+++ Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/security_group.tf
@@ -0,0 +1,27 @@
+# Security Group for Terra-VPC
+
+resource "aws_security_group" "allow-terra-ssh" {
+  name        = "allow_terra_ssh"
+  description = "security group to allow ssh connections"
+  vpc_id      = aws_vpc.terra-vpc.id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.terra-vpc.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.terra-vpc.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow-terra-ssh"
+  }
+}
--- Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/vpc.tf
+++ Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/vpc.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "terra-vpc" {
   instance_tenancy = "default"
   enable_dns_support = "true"
   enable_dns_hostnames = "true"
-  enable_classiclink = "false"
+  
 
   tags = {
     Name = "terra-vpc"
