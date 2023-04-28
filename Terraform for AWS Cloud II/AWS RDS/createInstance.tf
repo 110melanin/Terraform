e1dc7085f596515bb7c097d462234644a2311d6f
110melanin <justicegaisie141@gmail.com>
2023-04-28 03:18:53 +0100

    subnet and vpc sorted

--- Terraform for AWS Cloud II/AWS RDS/createInstance.tf
+++ Terraform for AWS Cloud II/AWS RDS/createInstance.tf
@@ -7,6 +7,7 @@ resource "aws_key_pair" "terra_key1" {
 resource "aws_instance" "terra-instance" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
+  availability_zone = "eu-west-2a"
   key_name      = aws_key_pair.terra_key1.key_name
   vpc_security_group_ids = [aws_security_group.allow-terra-ssh.id]
   subnet_id     = aws_subnet.terra-subnet-public-1.id
