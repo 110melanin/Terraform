87528e829eae3d3d2780a4fef4fd66000f07b8ee
110melanin <justicegaisie141@gmail.com>
2023-04-28 01:38:05 +0100

    cloudinit

--- Terraform for AWS Cloud/UserData/createInstance.tf
+++ Terraform for AWS Cloud/UserData/createInstance.tf
@@ -8,8 +8,6 @@ resource "aws_instance" "terra-instance" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
   key_name      = aws_key_pair.terra_key1.key_name
-
-  user_data     = file ("installapache.sh")
   
 
   tags = {
