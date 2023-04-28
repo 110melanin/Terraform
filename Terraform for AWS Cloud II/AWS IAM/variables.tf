6e828b4510223d6419fc2768b75568330c2de9cf
110melanin <justicegaisie141@gmail.com>
2023-04-28 04:20:19 +0100

    IAM

--- Terraform for AWS Cloud II/AWS IAM/variables.tf
+++ Terraform for AWS Cloud II/AWS IAM/variables.tf
@@ -1,32 +1,21 @@
 variable "AWS_ACCESS_KEY" {
     type = string
-    default = "AKIASMSIZOF42P2VUDSZ"
-}
+    default = "AKIAZXEUEPVWLUI5M2I5"
+    }
 
 variable "AWS_SECRET_KEY" {}
 
 variable "AWS_REGION" {
-default = "us-east-2"
+    default = "eu-west-2"
 }
-
 variable "AMIS" {
     type = map
     default = {
-        us-east-1 = "ami-0f40c8f97004632f9"
-        us-east-2 = "ami-05692172625678b4e"
-        us-west-2 = "ami-0352d5a37fb4f603f"
-        us-west-1 = "ami-0f40c8f97004632f9"
-    }
-}
-
-variable "PATH_TO_PRIVATE_KEY" {
-  default = "levelup_key"
+        eu-west-2 = "ami-032a2dcb7c12ce3c2"
+        
+ }
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "levelup_key.pub"
-}
-
-variable "INSTANCE_USERNAME" {
-  default = "ubuntu"
+    default = "terra_key1.pub"
 }
