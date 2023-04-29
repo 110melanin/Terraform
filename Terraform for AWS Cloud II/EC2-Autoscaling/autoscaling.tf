702e95d959fa7c80a0a0dc17d89e5bcce5c3739d
110melanin <justicegaisie141@gmail.com>
2023-04-29 05:45:01 +0100

    vpc mod

--- Terraform for AWS Cloud II/EC2-Autoscaling/autoscaling.tf
+++ Terraform for AWS Cloud II/EC2-Autoscaling/autoscaling.tf
@@ -25,8 +25,7 @@ resource "aws_autoscaling_group" "terra-autoscaling" {
   health_check_type         = "EC2"
   force_delete              = true
   launch_configuration      = aws_launch_configuration.terra-launchconfig.name
-  vpc_zone_identifier       = ["eu-west-2"]
-
+  vpc_zone_identifier       = aws_vpc.terra-vpc.id
    tag {
     key                 = "Name"
     value               = "terra custom EC2"
