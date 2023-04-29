e8d3699a291ac20422fff3bc5832ae9066de6ba3
110melanin <justicegaisie141@gmail.com>
2023-04-29 05:31:12 +0100

    key added

--- Terraform for AWS Cloud II/EC2-Autoscaling/createInstance.tf
+++ Terraform for AWS Cloud II/EC2-Autoscaling/createInstance.tf
@@ -1,9 +1,4 @@
 
-resource "aws_key_pair" "terra_key" {
-    key_name = "terra_key"
-    public_key = file(var.PATH_TO_PUBLIC_KEY)
-}
-
 resource "aws_instance" "terra-instance" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
