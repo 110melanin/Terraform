35808f0332abec497c481852c4ebb18025d42578
110melanin <justicegaisie141@gmail.com>
2023-04-30 03:56:20 +0100

    modules mod commit

---
+++ Terraform for AWS Cloud II/AWS-ELB/autoscaling.tf
@@ -0,0 +1,96 @@
+#keypair
+
+resource "aws_key_pair" "terra_key1" {
+    key_name = "terra_key1"
+    public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
+# Auto-Scaling Launch_configuration
+
+resource "aws_launch_configuration" "terra-launchconfig" {
+  name          = "terra-launchconfig"
+  image_id      = lookup(var.AMIS, var.AWS_REGION)
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.terra_key1.key_name
+}
+
+
+#AutoScaling Group
+
+resource "aws_autoscaling_group" "terra-autoscaling" {
+  name                      = "terra-autoscaling"
+  max_size                  = 2
+  min_size                  = 1
+  health_check_grace_period = 200
+  health_check_type         = "EC2"
+  force_delete              = true
+  launch_configuration      = aws_launch_configuration.terra-launchconfig.name
+  vpc_zone_identifier       = [aws_subnet.terra-subnet-public-1.id, aws_subnet.terra-subnet-public-2.id]
+   tag {
+    key                 = "Name"
+    value               = "terra custom EC2"
+    propagate_at_launch = false
+   }
+  }
+
+  #AutoScaling Policy
+
+  resource "aws_autoscaling_policy" "terra-autoscaling-policy" {
+  name                   = "terra-autoscaling-policy"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 200
+  autoscaling_group_name = aws_autoscaling_group.terra-autoscaling.name
+  policy_type            = "SimpleScaling"
+}
+
+# CloudWatch Monitoring
+
+resource "aws_cloudwatch_metric_alarm" "terra-alarm" {
+  alarm_name          = "terra-alarm"
+  alarm_description   = "Alarm once CPU usage Increase"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 30
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.terra-autoscaling.name
+  }
+
+  alarm_actions     = [aws_autoscaling_policy.terra-autoscaling-policy.arn]
+}
+
+# Auto Descaling Policy
+
+ resource "aws_autoscaling_policy" "terra-autoscaling-policy-scaledown" {
+  name                   = "terra-autoscaling-policy-scaledown"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 200
+  autoscaling_group_name = aws_autoscaling_group.terra-autoscaling.name
+  policy_type            = "SimpleScaling"
+}
+
+# Auto Descaling CloudWatch
+
+resource "aws_cloudwatch_metric_alarm" "terra-alarm-scaledown" {
+  alarm_name          = "terra-alarm-scaledown"
+  alarm_description   = "Alarm once CPU usage Decrease"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 10
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.terra-autoscaling.name
+  }
+
+  alarm_actions     = [aws_autoscaling_policy.terra-autoscaling-policy-scaledown.arn]
+}
--- Terraform_Modules/main.tf
+++ Terraform_Modules/main.tf
@@ -1,13 +1,16 @@
-module "ec2_cluster" {
-    source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
+module "ec2_instance" {
+  source  = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
 
-    name    = "my-cluster"
-    ami     = "ami-0f40c8f97004632f9"
-    instance_type          = "t2.micro"
-    subnet_id   = "subnet-e92f9cc8"
+  name = "terra-single-instance"
 
-    tags = {
+  ami                    = var.AMIS
+  instance_type          = "t2.micro"
+  key_name               = "justice"
+  monitoring             = false
+  subnet_id              = "subnet-0fce73655fe99a6ba"
+
+  tags = {
     Terraform   = "true"
     Environment = "dev"
-    }
+  }
 }
