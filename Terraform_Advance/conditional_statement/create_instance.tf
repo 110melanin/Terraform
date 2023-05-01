bebd11cc5f2ab2490ac13cc1c091eee1c2b09c3a
110melanin <justicegaisie141@gmail.com>
2023-05-01 02:34:37 +0100

    syntax

--- Terraform_Advance/conditional_statement/create_instance.tf
+++ Terraform_Advance/conditional_statement/create_instance.tf
@@ -7,8 +7,8 @@ module "ec2_instance" {
 
   name = "terra-single-instance"
 
-  ami                    =  "ami-032a2dcb7c12ce3c2"
-  instance_type          =  var.instance_type 
+  ami                    = "ami-032a2dcb7c12ce3c2"
+  instance_type          = var.instance_type 
   monitoring             = false
   subnet_id              = "subnet-0fce73655fe99a6ba"
   count                  = var.instance_type == "t2.micro" ? 2 : 1
