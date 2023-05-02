795c5fdbba8a45fef847c21f9ff55f121c5515e2
110melanin <justicegaisie141@gmail.com>
2023-05-02 02:22:26 +0100

    source id

--- Packer/Deploy_Custom_Image/custom_ami_deployment/createInstance.tf
+++ Packer/Deploy_Custom_Image/custom_ami_deployment/createInstance.tf
@@ -1,7 +1,7 @@
 # Create Instance using Custom VPC
 
 module "test-vpc" {
-  source      = module.terra-vpc
+  source      = module.terra-vpc.id
 
   ENVIRONMENT = var.ENVIRONMENT
   AWS_REGION  = var.AWS_REGION
